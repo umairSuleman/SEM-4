3224c82fb81ff8189eadce6a815290d4a1ba5169
umairSuleman <umair.mitmpl2023@learner.manipal.edu>
2025-04-01 19:42:21 +0530

    Create knapsackDP.c

---
+++ LABS/daa/LAB 10/knapsackDP.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// Function to return the maximum of two numbers
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// Function to solve 0/1 Knapsack using Dynamic Programming
+int knapsackDP(int p[], int w[], int maxCap, int n) {
+    int dp[n+1][maxCap+1];  // DP table
+
+    // Build the DP table
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= maxCap; j++) {  
+            // Base case: No items or capacity is 0
+            if (i == 0 || j == 0) {
+                dp[i][j] = 0;  
+            }
+            // If the current item's weight is within capacity
+            else if (j >= w[i-1]) {  
+                dp[i][j] = max(dp[i-1][j], p[i-1] + dp[i-1][j - w[i-1]]);
+            }
+            // Exclude the item
+            else {
+                dp[i][j] = dp[i-1][j];  
+            }
+        }
+    }
+
+    return dp[n][maxCap];  // Return the maximum profit
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int profits[n], weights[n], maxCap;
+
+    printf("Enter the weights of the objects: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &weights[i]);
+    }
+
+    printf("Enter the profits of the objects: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &profits[i]);
+    }
+
+    printf("Enter the maximum capacity: ");
+    scanf("%d", &maxCap);
+
+    int result = knapsackDP(profits, weights, maxCap, n);
+
+    printf("Maximised Profit: %d\n", result);
+    return 0;  
+}
