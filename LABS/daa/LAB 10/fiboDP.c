96786f83d90f746eb8cd4642994574af5e7cffba
umairSuleman <umair.mitmpl2023@learner.manipal.edu>
2025-04-01 17:05:21 +0530

    Create fiboDP.c

---
+++ LABS/daa/LAB 10/fiboDP.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int fibarr(int n) {
+    if (n <= 1)
+        return n;
+
+    int dp[n + 1];  // Array to store Fibonacci numbers
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+}
+
+int fibNoArray(int n) {
+    if (n <= 1)
+        return n;
+
+    int a = 0, b = 1, temp;
+    for (int i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+}
