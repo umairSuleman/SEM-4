624a23903db03dac2258315c117817b9013d890d
umairSuleman <umair.mitmpl2023@learner.manipal.edu>
2025-04-03 08:01:55 +0530

    Create distributionCounting.c

---
+++ LABS/daa/LAB 9/distributionCounting.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+void distributionCounting(int arr[], int n, int l, int u){
+
+    int S[n];                   //reult array
+    int numOfElements=u-l+1;       // number of elements
+
+    int D[numOfElements];       //Distribution Value array
+
+    // Initialise all values to zero
+    for(int i=0; i<numOfElements; i++){
+        D[i]=0;
+    }
+
+    //Computing frequencies
+    for(int i=0; i<n; i++){
+        D[arr[i]-l] += 1;
+    }
+
+    //Computing distribution values
+    for(int i=0; i<numOfElements; i++){
+        if(i==0){
+            continue;
+        }
+        else{
+            D[i]=D[i-1]+D[i]; //prev element D value + curr element freq value 
+        }
+    }
+
+    for(int i=n-1; i>=0; i--){
+        int j= arr[i]-l;
+        S[D[j]-1]=arr[i];
+        D[j]--;
+    }
+
+    printf("Sorrted Array:");
+    for(int i=0; i<n; i++)
+        printf("%d\t", S[i]);
+
+    printf("\n");
+}
+
+void main(){
+
+    int n; 
+    printf("Enter number of elements:");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:");
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+
+    int l, u; 
+    printf("Enter upper and lower limit:");
+    scanf("%d %d", &u, &l);
+
+    distributionCounting(arr, n, l, u);
+}
